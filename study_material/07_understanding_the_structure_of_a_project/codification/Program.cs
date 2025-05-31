555002ffe5cfa623ed5ca68a29a9d41b864decee
fzanneti <68480527+fzanneti@users.noreply.github.com>
2025-05-30 22:00:35 -0300

    Add: test project with pessoa class including properties and apresentar method

---
+++ study_material/07_understanding_the_structure_of_a_project/codification/Program.cs
@@ -0,0 +1,21 @@
+﻿using codification.models;
+
+Pessoa pessoa = new Pessoa
+{
+    Nome = "",
+    Idade = 0
+};
+
+Pessoa pessoa1 = new Pessoa
+{
+    Nome = "",
+    Idade = 0
+};
+
+pessoa.Nome = "Fabio Zanneti";
+pessoa.Idade = 44;
+pessoa.Apresentar();
+Console.WriteLine($"");
+pessoa1.Nome = "John Doe";
+pessoa1.Idade = 30;
+pessoa1.Apresentar();                 
---
+++ study_material/07_understanding_the_structure_of_a_project/codification/models/Pessoa.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace codification.models
+{
+
+    public class Pessoa
+    {
+
+        public required string Nome { get; set; }
+        public required int Idade { get; set; }
+
+        public void Apresentar()
+        {
+
+            Console.WriteLine($"Olá prazer em conhecê-lo. Meu nome é {Nome}. Tenho {Idade} anos");
+            Console.WriteLine($"Hello, nice to meet you. My name is {Nome}. I'm {Idade} years old.");
+
+        }
+        
+    }
+}
