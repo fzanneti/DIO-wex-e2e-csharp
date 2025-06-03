c5b663490d8594e16e0f83998459a31308937c2b
Fabio Zanneti da Silva <fzanneti@gmail.com>
2025-06-03 12:53:33 -0300

    Update

---
+++ study_material/17_exceptions_and_collections_with_csharp/codification/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Globalization;
+
+string[] linhas = File.ReadAllLines("assets/arquivoLeitura.txt"); // Lê todas as linhas do arquivo encontrado no caminho especificado
+
+foreach (string linha in linhas) // Itera sobre cada linha do arquivo
+{
+   Console.WriteLine(linha); // Exibe a linha no console
+}
