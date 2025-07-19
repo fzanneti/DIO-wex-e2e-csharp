5606fc4aec7a17712f508107a314e5e3ed0beb97
Fabio Zanneti da Silva <fzanneti@gmail.com>
2025-07-19 19:01:02 -0300

    Update

--- study_project/project_6/DesafioWEXDockerCompose/JarbasBot/obj/Debug/net8.0/JarbasBot.AssemblyInfo.cs
+++ study_project/project_6/DesafioWEXDockerCompose/JarbasBot/obj/Debug/net8.0/JarbasBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JarbasBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71cc7fc1d1299f958c94897da2a8fb894f668544")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8ca646982ee8e5ed53cce37337bfa647be44c6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("JarbasBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JarbasBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
