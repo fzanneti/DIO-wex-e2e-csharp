b9000c352a58b0cea4118a2b9120a1cb26730aa4
Fabio Zanneti da Silva <fzanneti@gmail.com>
2025-07-21 05:11:28 -0300

    Update

--- study_project/project_6/DesafioWEXDockerCompose/JarbasBot/Models/ChatRequest.cs
+++
@@ -1,7 +0,0 @@
-namespace JarbasBot.Models
-{
-    public class ChatRequest
-    {
-        public string Question { get; set; } = string.Empty;
-    }
-}
--- study_project/project_6/DesafioWEXDockerCompose/JarbasBot/Models/ChatResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace JarbasBot.Models
-{
-    public class ChatResponse
-    {
-        public string Answer { get; set; } = string.Empty;
-    }
-}
-
